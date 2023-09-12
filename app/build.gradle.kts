aef1cbd1009049be68699acc5a84e68d1cc30140
regina <usmanova.re@yandex.ru>
2023-09-12 21:15:59 +0500

    Добавила Toolbar , библиотеки подправила и размеры текста

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,4 +39,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+        //вот эту строку добавила для зависимости на библиотеку "androidx.appcompat:appcompat" в проекте
+    implementation ("androidx.appcompat:appcompat:1.3.0")
 }
--- app/src/main/java/com/example/cafeor/LoginActivity.java
+++ app/src/main/java/com/example/cafeor/LoginActivity.java
@@ -1,14 +1,18 @@
 package com.example.cafeor;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.widget.Toolbar;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
 
+public class LoginActivity extends AppCompatActivity {
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        setTitle("Hi"); //добавила отсюда, так как в xml не реагирует
     }
 }

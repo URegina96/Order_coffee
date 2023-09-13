df421e7258c3f400187ef99ba7fd6d4fe5ae0a7f
regina <usmanova.re@yandex.ru>
2023-09-13 10:33:03 +0500

    Создала CreateOrder –    вторая активность,    запускающаяся при button

---
+++ app/src/main/java/com/example/cafeor/CreateOrderActivity.java
@@ -0,0 +1,14 @@
+package com.example.cafeor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateOrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_order);
+    }
+}
--- app/src/main/java/com/example/cafeor/LoginActivity.java
+++ app/src/main/java/com/example/cafeor/LoginActivity.java
@@ -2,20 +2,48 @@ package com.example.cafeor;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText editTextName; //переменные для доступа к графическим элементам
+    private EditText editTextPassword;//переменные для доступа к графическим элементам
     private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         setTitle("Hi, I love coffe and you?"); //добавила в эту строку, так как в xml не реагирует
         toolbar.setLogo(R.drawable.baseline_coffee_maker_24);
         toolbar.setTitleTextColor(0xFFFFFFFF);//добавила в эту строку, так как в xml не реагирует
 
+        //присваиваем им значения
+        editTextName = findViewById(R.id.editTextTextName);
+        editTextPassword =findViewById(R.id.editTextPassword);
     }
+    public void onClickCreateOrder(View view){
+        //получаем текст введенный пользователем, преобразовывая сразу в строку
+        String name=editTextName.getText().toString().trim();
+        //.trim() - избавляемся от лишних пробелов (до и после допустим)
+        String password =editTextPassword.getText().toString().trim();
+        //теперь запускаем новую активность только когда name и password содержат  какие-либо символы
+        if (!name.isEmpty()&&!password.isEmpty()){
+            Intent intent =new Intent(this,CreateOrderActivity.class); //вызываем новую активность
+
+            // вкалдываем данные в intent
+            intent.putExtra("name",name);
+            intent.putExtra("password",password);
+
+            startActivity(intent); //запускаем новую активнсть
+        }
+    }
+
 }
